087994e81b9ad7b12b6ca650498ee97eb21e8440
vitoriaeliza78 <vitoria69796@estudante.ifb.edu.br>
2025-11-18 20:14:55 -0300

    criei o service

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { About } from "./_components/about";
 import { Hero } from "./_components/hero";
+import { Servico } from "./_components/servico";
 
 export default function Home(){
   return(
     <main>
       <Hero/>
       <About/>
+      <Servico/>
     </main>
   )
 }
