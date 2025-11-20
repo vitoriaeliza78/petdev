4b87c9188d62327200b9f156563b1901598437f3
vitoriaeliza78 <vitoria69796@estudante.ifb.edu.br>
2025-11-20 08:40:39 -0300

    criando o service

---
+++ app/_components/service.tsx
@@ -0,0 +1,11 @@
+export function Service() {
+    return(
+        <section className="bg-white py-16">
+            <div className="container mx-auto px-4">
+                <div>
+                    <h2 className="">Nossos Serviços</h2>
+                </div>
+            </div>
+        </section>
+    )
+}
