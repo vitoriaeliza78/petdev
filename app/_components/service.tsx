92df70196986c648f82e78cfbce59dd4e1b80333
vitoriaeliza78 <vitoria69796@estudante.ifb.edu.br>
2025-11-20 08:55:45 -0300

    embla-carousel

--- app/_components/service.tsx
+++ app/_components/service.tsx
@@ -1,10 +1,15 @@
+"use client";
+import u from 'embla-carousel-react';
+
 export function Service() {
     return(
         <section className="bg-white py-16">
             <div className="container mx-auto px-4">
-                <div>
-                    <h2 className="text-4xl">Nossos Serviços</h2>
-                </div>
+            
+                    <h2 className="text-4xl font-bold mb-12">Nossos Serviços</h2>
+            <div className="relative'">
+
+            </div>
             </div>
         </section>
     )
