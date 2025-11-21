0f152b837b006145ecd47cd94de8d5e1dc8914b8
vitoriaeliza78 <vitoria69796@estudante.ifb.edu.br>
2025-11-21 15:32:01 -0300

    criando os botões laterais dos services

--- app/_components/service.tsx
+++ app/_components/service.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel } from 'lucide-react';
+import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel, Clock } from 'lucide-react';
 import useEmblaCarousel from 'embla-carousel-react';
 import { WhatsappLogoIcon } from '@phosphor-icons/react';
 import { title } from 'process';
@@ -53,6 +53,16 @@ export function Service() {
         }
     })
 
+
+    function scrollPrev() {
+        emblaApi?.scrollPrev();
+    }
+
+     function scrollNext() {
+        emblaApi?.scrollNext();
+    }
+
+
     return (
         <section className="bg-white py-16">
             <div className="container mx-auto px-4">
@@ -67,21 +77,48 @@ export function Service() {
                                 <div key={index} className="flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3">{/*Pega 100% da largura no mobile e 1/3 no desktop* do espaço quando mobile*/}
                                     <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col'>
                                         <div className='flex-1 flex items-start justify-between'>
-                                            <div>
+
+                                            <div className='flex gap-3'>
                                                 <span className='text-3xl'>{item.icon}</span>
                                                 <div>
-                                                    <h3 className='font'>{item.title}</h3>
-                                                    <p>
+                                                    <h3 className='font-bold text-xl my-1'>{item.title}</h3>
+                                                    <p className='text-gray-400 text-sm select-none'>
                                                         {item.description}
                                                     </p>
                                                 </div>
                                             </div>
+                                        </div>
+
+                                        {/* Rodapé com relógio */}
+                                        <div className='border-t border-gray-700 pt-4 flex items-center justify-between'>
+                                            <div className='flex items-center gap-2 text-sm'>
+                                                <Clock className='w-4 h-4' />
+                                                <span>{item.duration}</span>
+                                            </div>
+
+                                            {/* Link do WhatsApp */}
+                                            <a
+                                                href="#"
+                                                className='flex items-center justify-center gap-2 hover:bg-red-600 p-2 rounded-lg duration-300'
+                                            >
+                                                <WhatsappLogoIcon className='w-5 h-5'/>
+                                                Entrar em contato
+                                            </a>
                                         </div>
                                     </article>
                                 </div>
                             ))}
                         </div>
                     </div>
+
+                    {/* Botões de navegação */}
+
+                    <button className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute
+                    left-1 -translate-y-1/2 -translate-x-1/2 top-1/2'
+                    onClick={scrollPrev}>
+                        <ChevronLeft 
+                        className='w-6 h-6 text-gray-600 ' />
+                    </button>
                 </div>
             </div>
         </section>
