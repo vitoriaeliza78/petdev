c2494faa0aabd691195202667988c824fc6c5cbb
vitoriaeliza78 <vitoria69796@estudante.ifb.edu.br>
2025-11-22 20:26:21 -0300

    colocando as marcas

---
+++ app/_components/footer.tsx
@@ -0,0 +1,23 @@
+import golden from '../../public/golden.png'
+import natural from '../../public/natural.png'
+import primier from '../../public/primier.png'
+import royal from '../../public/royal.png'
+import whiskas from '../../public/whiskas.png'
+
+
+const brands = [
+    {name: 'Royal Canin', logo: royal},
+    {name: 'Golden', logo: golden},
+    {name: 'Primier', logo: primier},
+    {name: 'Formula Natural', logo: natural},
+    {name: 'Whiskas', logo: whiskas},
+    {name: 'Golden', logo: golden},
+]
+
+export function Footer(){
+    return(
+        <section className=''>
+
+        </section>
+    )
+}
